2f386771f5f4bcebb5679b40f57cae957cb8dd4e
Rushendra Reddy <rushendrareddy.d@gmail.com>
2024-07-14 17:55:59 +0530

    Added icons based on Weather conditions Added sun rise and sun set timings Added a 7 day Weather Forecast display Replace the API to get 7 days weather forecast as the provides API is a basic plan

--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const WeatherCard = ({ data }) => {
   const { main, weather, wind } = data;
+  const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
   return (
     <div className="weather-card">
+      <img src={iconUrl} alt={weather[0].description} />
       <p>Temperature: {main.temp} °C</p>
       <p>Humidity: {main.humidity} %</p>
       <p>Conditions: {weather[0].description}</p>
